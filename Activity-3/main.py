ece6dadd9406047c1f002048e7bb627a3b428195
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-08-20 17:12:27 +0530

    Activity-3

---
+++ Activity-3/main.py
@@ -0,0 +1,14 @@
+first_name = "Akshat"
+last_name="Agrawal"
+full_name=first_name+last_name
+example="Haa"*5
+print("First Name :",first_name)
+print("Last Name :",last_name)
+print("Full Name :",full_name)
+print("String Multiplied 5 times gives this result :",example)
+
+word='Coding'
+print("Length of String :", len(word))
+print("First letter of String :", word[0])
+print("Last letter of String :", word[5])
+print("Sliced String :", word[0:3])
