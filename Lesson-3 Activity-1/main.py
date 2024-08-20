2e89ddaee3dfec15054fd922727ba004c486a0ad
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-08-20 19:42:05 +0530

    Activity-3 Lesson-3

--- Lesson-3 Activity-1/main.py
+++ Lesson-3 Activity-1/main.py
@@ -3,3 +3,4 @@ if num % 2 == 0:
   print("Given number is Even") 
 else: 
   print("Given number is Odd")
+
---
+++ Lesson-3 Activity-3/Activity-3/main.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a num:"))
+if num>50:
+        print("Num is greater than 50.")
+        if num%2==0:
+                print("And even num too.")
+        else:
+                print("And odd num too.")
+else:
+    print("Num is less than 50")
