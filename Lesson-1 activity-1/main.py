e941966949b3dcbf69981045aedcc371b3a8dd4e
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-08-20 10:24:32 +0530

    Message

--- Lesson-1 activity-1/main.py
+++ Lesson-1 activity-1/main.py
@@ -1,1 +1,13 @@
 print("Hello World")
+name= "Penguin"
+age= 15
+
+print("Name :",name)
+print("Data Type Name is",type(name))
+print("Age :",age)
+print("Data Type Age is", type(age))
+
+print("/n After Type Casting...")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
