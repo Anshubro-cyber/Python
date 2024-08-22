fe01852720cd85da2a44c88bfd6e1403ba21e591
Anshubro-cyber <akshatagrawal0831@gmail.com>
2024-08-22 16:48:47 +0530

    ACP-2

---
+++ ACP-2/main.py
@@ -0,0 +1,11 @@
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+c = int(input("Enter the value of c:"))
+print("After Swapping the Values are:")
+temp = a
+a = b
+b = c
+c = temp
+print("The value of a after swapping:",a)
+print("The value of b after swapping:",b)
+print("The value of c after swapping:",c)100
